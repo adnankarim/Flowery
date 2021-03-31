e07b4bc6befa37b20310eaee67a4466027d4435f
adnankarim <skiwordylectures@gmail.com>
2021-03-31 21:24:45 +0500

    basic styling completed

--- App.js
+++ App.js
@@ -1,51 +1,51 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { Image } from 'react-native-elements';
 import { Button } from './components/button/Button';
 class App extends Component {
   render() {
     return (
-      <LinearGradient style={{ flex: 1, flexDirection: "row" }} colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.linearGradient}>
-        <Text style={styles.buttonText}>
-          Flowery
-          </Text>
-        <View style={{ alignItems: 'center', justifyContent: 'center', marginTop: 100 }} >
-          <Image source={require('./assets/flower.png')} style={{ width: 250, height: 250, padding: 5 }}>
-          </Image>
+      <LinearGradient
+        style={{ flex: 1, flexDirection: 'row' }}
+        colors={['#4c669f', '#3b5998', '#192f6a']}
+        style={styles.linearGradient}>
+        <Text style={styles.buttonText}>Flowery</Text>
+        <View
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop: 250,
+          }}>
+          <Image
+            source={require('./assets/flower.png')}
+            style={{ width: 250, height: 250, padding: 5 }}></Image>
         </View>
-        <View style={{ flex: 1, marginHorizontal: 30, marginTop: 80, fontSize: 25, }}>
-          <Button title={"NN1"} style={{ color: 'black', }}>
-
-          </Button>
-          <Button title={"NN1"} style={{ marginTop: 10 }}>
-
-          </Button>
+        <View
+          style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-end', marginHorizontal: 30, margin: 80, fontSize: 25 }}>
+          <Button title={'Take a Photo'} style={{ width: 250, backgroundColor: 'black' }}></Button>
+          <Button title={'Select an Image'} style={{ marginTop: 10, width: 250, backgroundColor: 'black' }}></Button>
         </View>
-        <View>
-
-        </View>
-      </LinearGradient >
+      </LinearGradient>
     );
   }
 }
 
 export default App;
 
-
 var styles = StyleSheet.create({
   linearGradient: {
     flex: 1,
     paddingLeft: 15,
     paddingRight: 15,
-    borderRadius: 5
+    borderRadius: 5,
   },
   buttonText: {
     fontSize: 28,
     fontFamily: 'Gill Sans',
     textAlign: 'center',
     margin: 10,
-    marginTop: 20,
+    marginTop: 50,
     color: '#ffffff',
     backgroundColor: 'transparent',
   },
--- components/button/Button.js
+++ components/button/Button.js
@@ -36,21 +36,3 @@ const styles = StyleSheet.create({
 });
 
 
-
-
-// // - Example of using - your-screen.js
-
-// import React from 'react';
-// import { View } from 'react-native';
-// import { CustomButton } from './custom-button.js';
-
-// export const MyScreen = () => (
-//     <View>
-//         <UIButton
-//             title="Open alert!"
-//             onPress={() => Alert.alert(`Why you opened me? Go away, it's mine!`)}
-//             style={{ /* some styles for button */ }}
-//             textStyle={{ /* styles for button title */ }}
-//         />
-//     </View>
-// );
